bfd3d80a9b95ef74145cfb5e8b50549c5ca8231a
guilapabsi <guilapabsi@gmail.com>
2014-01-03 12:27:48 +0000

    criação de classes.

---
+++ modelo/tiposobjetos/Endereco.java
@@ -0,0 +1,46 @@
+package tiposobjetos;
+
+public class Endereco {
+	private String rua;
+	private String bairro;
+	private String complemento;
+	private String cidade;
+	private String cep;
+	private int numero;
+	
+	public Endereco() {
+		
+	}
+	
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+}
