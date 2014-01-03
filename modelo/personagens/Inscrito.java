a9b9c256d997df6b957dbf7f62e7b7f3259c2c3a
guilapabsi <guilapabsi@gmail.com>
2014-01-03 12:27:01 +0000

    criação de classes.

---
+++ modelo/personagens/Inscrito.java
@@ -0,0 +1,21 @@
+package personagens;
+
+import tiposobjetos.TipoSituacaoInscrito;
+
+public class Inscrito {
+	private int codigo;
+	private TipoSituacaoInscrito situacaoInscrito;
+	
+	public Inscrito() {
+		
+	}
+
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+}
