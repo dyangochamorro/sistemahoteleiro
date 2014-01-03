ff3c23089571996f834ed733e376865a5581096e
guilapabsi@gmail.com <guilapabsi@gmail.com@e6b0dfca-0b27-c389-8045-aecd0db72437>
2014-01-03 12:21:18 +0000

    criação de classes.

---
+++ trunk/modelo/financeiro/FaturaHospede.java
@@ -0,0 +1,32 @@
+package financeiro;
+
+import java.util.ArrayList;
+
+public class FaturaHospede {
+	private ArrayList<Debito> debitos;
+	private boolean situacao;
+	
+	public FaturaHospede() {
+		
+	}
+	
+	public boolean isSituacao() {
+		return situacao;
+	}
+	public void setSituacao(boolean situacao) {
+		this.situacao = situacao;
+	}
+	
+	public void verificaSituacaoDebitos() {
+		
+	}
+	
+	public void fechaFatura() {
+		
+	}
+	
+	public void criaFatura() {
+		
+	}
+
+}
