a578eac7d94aee77a8b428801b0237725f015502
guilapabsi@gmail.com <guilapabsi@gmail.com@e6b0dfca-0b27-c389-8045-aecd0db72437>
2014-01-03 12:25:23 +0000

    criação de classes.

---
+++ trunk/modelo/personagens/Usuario.java
@@ -0,0 +1,41 @@
+package personagens;
+
+
+public class Usuario {
+	private String descricao;
+	private String senha;
+	private int codigo;
+	
+	
+	public Usuario() {
+		
+	}
+	
+	public void login() {
+		
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+}
