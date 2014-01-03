f789ed27061c84eb63db0b579a2877a88e25a4a5
guilapabsi@gmail.com <guilapabsi@gmail.com@e6b0dfca-0b27-c389-8045-aecd0db72437>
2014-01-03 12:22:11 +0000

    criação de classes.

---
+++ trunk/modelo/financeiro/Debito.java
@@ -0,0 +1,24 @@
+package financeiro;
+
+public class Debito {
+	private float valor;
+	private String detalhes;
+	
+	public Debito() {
+		
+	}
+	
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public String getDetalhes() {
+		return detalhes;
+	}
+	public void setDetalhes(String detalhes) {
+		this.detalhes = detalhes;
+	}
+
+}
