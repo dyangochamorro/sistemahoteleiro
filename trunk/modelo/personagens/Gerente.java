ecb6c9a46321c1cc4ffb8ea80b3107a61f254843
guilapabsi@gmail.com <guilapabsi@gmail.com@e6b0dfca-0b27-c389-8045-aecd0db72437>
2014-01-03 12:24:00 +0000

    criação de classes.

---
+++ trunk/modelo/personagens/Gerente.java
@@ -0,0 +1,23 @@
+package personagens;
+
+public class Gerente extends Funcionario {
+	
+	private Recepcionista recepcionista;
+	
+	public Gerente() {
+		
+	}
+	
+	public void cadastrarUsuario() {
+		
+	}
+	
+	public void cadastrarTipoUsuario() {
+		
+	}
+	
+	public void cadastrarEvento() {
+		
+	}
+
+}
