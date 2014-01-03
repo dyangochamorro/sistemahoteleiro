3fef9fc1baa140cc91ed5b31d65ef753ff6ae128
guilapabsi@gmail.com <guilapabsi@gmail.com@e6b0dfca-0b27-c389-8045-aecd0db72437>
2014-01-03 12:26:06 +0000

    criação de classes.

---
+++ trunk/modelo/personagens/Funcionario.java
@@ -0,0 +1,27 @@
+package personagens;
+
+public class Funcionario {
+	private String matricula;
+	private float salario;
+	private Usuario usuario;
+	
+	public Funcionario() {
+		
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	
+	public float getSalario() {
+		return salario;
+	}
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+
+}
