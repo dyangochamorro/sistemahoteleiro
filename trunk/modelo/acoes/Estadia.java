ed11b01190a1681e3ef8124d1e973b97642d48e7
guilapabsi@gmail.com <guilapabsi@gmail.com@e6b0dfca-0b27-c389-8045-aecd0db72437>
2014-01-03 12:18:44 +0000

    criação de classes.

---
+++ trunk/modelo/acoes/Estadia.java
@@ -0,0 +1,34 @@
+package acoes;
+
+import java.util.Date;
+
+public class Estadia {
+	
+	private Date dataInicio;
+	private Date dataFim;
+	
+	public Date getDataInicio() {
+		return dataInicio;
+	}
+
+	
+	public void setDataInicio(Date dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	
+	public Estadia() {
+		
+	}
+	
+	
+
+}
