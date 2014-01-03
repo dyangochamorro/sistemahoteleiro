1f4881668eb6f98088c7951594a193979880a570
guilapabsi <guilapabsi@gmail.com>
2014-01-03 12:25:43 +0000

    criação de classes.

---
+++ modelo/personagens/Recepcionista.java
@@ -0,0 +1,23 @@
+package personagens;
+
+import ambientes.Suite;
+
+public class Recepcionista {
+	
+	public Recepcionista() {
+		
+	}
+	
+	public void checkIn(Hospede hospede, Suite suite) {
+		
+	}
+	
+	public void checkOut(Hospede hospede, Suite suite) {
+		
+	}
+	
+	public void cadastrarHospede(Hospede hospede) {
+		
+	}
+
+}
